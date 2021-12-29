eb4a66e0c430f649ff6b65923c5776d39e5dab9b
Buck Randall <tbr446@gmail.com>
2021-12-28 20:48:48 -0600

    tue

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-/* eslint-disable no-prototype-builtins */
 import { Header, Nav, Main, Footer } from "./components";
 import * as state from "./store";
 import axios from "axios";
+
 import Navigo from "navigo";
 import { capitalize } from "lodash";
+
 import dotenv from "dotenv";
 dotenv.config();
 
 const router = new Navigo(window.location.origin);
+
+//Move router to the end AND add Router.Hooks
 
 function render(st) {
   document.querySelector("#root").innerHTML = `
@@ -17,10 +20,13 @@ function render(st) {
     ${Footer()}
   `;
   router.updatePageLinks();
-  addEventListeners();
+
+  addEventListeners(st);
 }
 
+//  FUNCTION FOR EVENT LISTENERS
 function addEventListeners(st) {
+  // add event listeners to Nav items for navigation
   document.querySelectorAll("nav a").forEach(navLink =>
     navLink.addEventListener("click", event => {
       event.preventDefault();
@@ -34,8 +40,47 @@ function addEventListeners(st) {
     .addEventListener("click", () =>
       document.querySelector("nav > ul").classList.toggle("hidden--mobile")
     );
+
+  if (st.view === "Order") {
+    document.querySelector("form").addEventListener("submit", event => {
+      event.preventDefault();
+
+      const inputList = event.target.elements;
+      console.log("Input Element List", inputList);
+
+      const toppings = [];
+      // Interate over the toppings input group elements
+      for (let input of inputList.toppings) {
+        // If the value of the checked attribute is true then add the value to the toppings array
+        if (input.checked) {
+          toppings.push(input.value);
+        }
+      }
+
+      const requestData = {
+        customer: inputList.customer.value,
+        crust: inputList.crust.value,
+        cheese: inputList.cheese.value,
+        sauce: inputList.sauce.value,
+        toppings: toppings
+      };
+      console.log("request Body", requestData);
+
+      axios
+        .post(`${process.env.PIZZA_PLACE_API_URL}`, requestData)
+        .then(response => {
+          // Push the new pizza onto the Pizza state pizzas attribute, so it can be displayed in the pizza list
+          state.Pizza.pizzas.push(response.data);
+          router.navigate("/Pizza");
+        })
+        .catch(error => {
+          console.log("It puked", error);
+        });
+    });
+  }
 }
 
+//  ADD ROUTER HOOKS HERE ...
 router.hooks({
   before: (done, params) => {
     const page =
@@ -46,7 +91,7 @@ router.hooks({
     if (page === "Home") {
       axios
         .get(
-          `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st.%20louis`
+          `https://api.openweathermap.org/data/2.5/weather?appid=fae9f2b427330bcc41ed33840b06167e&q=st.%20louis`
         )
         .then(response => {
           state.Home.weather = {};
@@ -73,12 +118,10 @@ router.hooks({
   }
 });
 
+//ADD ROUTER HERE ...
 router
   .on({
     "/": () => render(state.Home),
-    ":page": params => {
-      let page = capitalize(params.page);
-      render(state[page]);
-    }
+    ":page": params => render(state[capitalize(params.page)])
   })
   .resolve();
---
+++ server/index.js
@@ -0,0 +1,69 @@
+// const http = require("http");
+// const server = http.createServer((request, response) => {
+//   if (request.url === "/status" && request.method === "GET") {
+//     response.writeHead(200, { "Content-Type": "application/json" });
+//     response.write(JSON.stringify({ message: "Service am healthy" }));
+//     response.end();
+//   }
+// });
+
+// server.listen(4040);
+// console.log("Listening on port 4040");
+//====================================================================================================
+// 'Import' the Express module instead of http
+
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const pizzas = require("./routers/pizzas");
+// Initialize the Express application
+const app = express();
+
+dotenv.config();
+mongoose.connect(process.env.MONGODB);
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
+
+// import mongoose
+
+const logging = (request, response, next) => {
+  console.log(`${request.method} ${request.url} ${Date.now()}`);
+  next();
+};
+
+// CORS Middleware
+const cors = (req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept,Authorization,Origin"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+};
+
+app.use(cors);
+app.use(express.json());
+app.use(logging);
+app.use("/pizzas", pizzas);
+
+// Handle the request with HTTP GET method from http://localhost:4040/status
+app.get("/status", (request, response) => {
+  // Create the headers for response by default 200
+  // Create the response body
+  // End and return the response
+  response.send(JSON.stringify({ message: "Service healthy" }));
+});
+
+// Tell the Express app to start listening
+// Let the humans know I am running and listening on 4040
+app.listen(4040, () => console.log("Listening on port 4040"));
